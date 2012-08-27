64c2447e6b8400c13086880c08743949914b6114
Sayan Chakrabarti <me@sayanriju.co.cc>
2012-08-27 19:54:04 +0530

    New fullpage screenie of Admin CP

--- sys/gui/pages/error.php
+++ sys/gui/pages/error.php
@@ -4,7 +4,8 @@
 <head>
 	<meta charset="utf-8">
 	<title><?php echo $error['code'] ;?> Error: NoFrills CMS</title>
-	<link rel='stylesheet' href='assets/css/bootstrap.css'/>
+	<link rel='stylesheet' href='assets/css/bootstrap.min.css'/>
+	<link rel='stylesheet' href='assets/css/bootstrap-responsive.min.css'/>
 	<link rel='stylesheet' href='assets/css/style.css'/>
 </head>
 <body>
--- sys/gui/pages/welcome.php
+++ sys/gui/pages/welcome.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>NoFrills CMS</title>
-	<link rel='stylesheet' href='assets/css/bootstrap.css'/>
+	<link rel='stylesheet' href='assets/css/bootstrap.min.css'/>
+  	<link rel='stylesheet' href='assets/css/bootstrap-responsive.min.css'/>
 	<link rel='stylesheet' href='assets/css/style.css'/>
 </head>
 <body>
